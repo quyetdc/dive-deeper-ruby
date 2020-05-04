68462b18aca069af96d8be4997b42d3e9d3798d8
quyetdc <quyetdc.uet@gmail.com>
2020-05-04 15:31:20 +0700

    modules

---
+++ modules.rb
@@ -0,0 +1,47 @@
+# The main purpose of modules is to group together methods and constants. 
+# One difference with classes is that you can't create instances of a module.
+
+module Numbers 
+	PI = 3.141592
+end
+#=> puts Numbers::PI
+
+module Numbers
+	def self.double(number)
+		number * 2 
+	end
+end
+puts Numbers.double(30)
+
+
+module Life 
+end
+
+class Animal 
+	include Life
+end
+
+
+module ClassMethods 
+	def add_product
+		puts 'Add Product'
+	end
+end
+
+class Store
+	# Add methods from the module to the singleton class of this object 
+	extend ClassMethods
+end 
+
+Store.add_product
+
+
+ 
+module Formatter 
+	extend self
+	def wrap_with(symbol, str) 
+		"#{symbol * 4} #{str} #{symbol * 4}"
+	end 
+end
+
+puts Formatter.wrap_with("*", "hello")
